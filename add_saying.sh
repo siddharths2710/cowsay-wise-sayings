b435f1b23e59499c6f32c93708e823579e8da434
Siddharth <siddharths2710phone@gmail.com>
2019-02-27 07:05:10 -0700

    added new saying 25288

--- add_saying.sh
+++ add_saying.sh
@@ -1,6 +1,12 @@
-docker run --name sayings ujjwal9695/cowsay-wise-sayings >> /home/appsadm/cowsay-wise-sayings/saying_main.txt
+RANDOM=$$
+if [ `expr $RANDOM % 2` -eq 0 ]
+then
+	docker run --name sayings ujjwal9695/cowsay-wise-sayings >> /home/appsadm/cowsay-wise-sayings/README.md
+else
+	docker run --name sayings chuanwen/cowsay >> /home/appsadm/cowsay-wise-sayings/README.md 
+fi
 docker rm sayings
 cd /home/appsadm/cowsay-wise-sayings/
 git add .
-git commit -m "added new saying"
+git commit -m "added new saying $RANDOM"
 git push origin master
