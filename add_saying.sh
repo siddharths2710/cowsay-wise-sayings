7a2237626ccf5f8ffc7bd6c1be6a88433a1316ce
root <siddharths2710@yahoo.com>
2020-03-03 16:54:31 +0100

    added new saying 14869

--- add_saying.sh
+++ add_saying.sh
@@ -1,34 +1,34 @@
 RANDOM=$$
-echo "\`\`\`" >> /root/Documents/cowsay/cowsay-wise-sayings/README.md
+echo "\`\`\`" >> /root/Documents/cowsay-wise-sayings/README.md
 REM=`expr $RANDOM % 7`
 case $REM in
 0)
-	fortune | cowsay -f snowman >> /root/Documents/cowsay/cowsay-wise-sayings/README.md
+	fortune | cowsay -f snowman >> /root/Documents/cowsay-wise-sayings/README.md
 ;;
 1)
-	fortune | cowsay -f tux >> /root/Documents/cowsay/cowsay-wise-sayings/README.md
+	fortune | cowsay -f tux >> /root/Documents/cowsay-wise-sayings/README.md
 ;;
 2)
-	fortune | cowsay -f turtle >> /root/Documents/cowsay/cowsay-wise-sayings/README.md
+	fortune | cowsay -f turtle >> /root/Documents/cowsay-wise-sayings/README.md
 ;;
 3)
-	fortune | cowsay -f stimpy >> /root/Documents/cowsay/cowsay-wise-sayings/README.md
+	fortune | cowsay -f stimpy >> /root/Documents/cowsay-wise-sayings/README.md
 ;;
 4)
-	fortune | cowsay -f dragon >> /root/Documents/cowsay/cowsay-wise-sayings/README.md
+	fortune | cowsay -f dragon >> /root/Documents/cowsay-wise-sayings/README.md
 ;;
 5)
-	fortune | cowsay -f elephant >> /root/Documents/cowsay/cowsay-wise-sayings/README.md
+	fortune | cowsay -f elephant >> /root/Documents/cowsay-wise-sayings/README.md
 ;;
 6)
-	fortune | cowsay -f calvin >> /root/Documents/cowsay/cowsay-wise-sayings/README.md
+	fortune | cowsay -f calvin >> /root/Documents/cowsay-wise-sayings/README.md
 ;;
 *)
-	fortune | cowsay >> /root/Documents/cowsay/cowsay-wise-sayings/README.md
+	fortune | cowsay >> /root/Documents/cowsay-wise-sayings/README.md
 ;;
 esac
-echo "\`\`\`" >> /root/Documents/cowsay/cowsay-wise-sayings/README.md
-cd /root/Documents/cowsay/cowsay-wise-sayings/
+echo "\`\`\`" >> /root/Documents/cowsay-wise-sayings/README.md
+cd /root/Documents/cowsay-wise-sayings/
 git add .
 git commit -m "added new saying $RANDOM"
 git push origin master
